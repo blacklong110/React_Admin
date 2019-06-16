4f0110c9400e7e3459369ee6db6919352991110c
yukuri <250433683@qq.com>
2019-06-16 18:01:05 +0800

    login page done

---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+import ajax from './ajax'
+
+/*
+export function reqLogin(username, password) {
+    return ajax('/login', {username, password}, 'POST')
+}*/
+
+
+//export const reqLogin = (username, password) => ajax('/login', {username, password}, 'POST')
+export const reqLogin = (user) => ajax('/login', user, 'POST')
+
+
+
+
